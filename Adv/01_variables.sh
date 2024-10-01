50cea33aef64b99a647fa7b5893e9d9dd637b2e8
Manoranjan Sethi <manoranjansethi97@gmail.com>
2024-10-01 23:48:34 +0530

    Str

---
+++ Adv/01_variables.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+name="Mano"
+echo "Hello $name!"
+
+x=8
+y=9
+sum=$((x+y))
+echo "Sum : $sum"
+
+a="Hello"
+b="World"
+
+c=$a,$b
+echo "Message : $c"
+
+string="Mano Ranjan"
+length=${#string}
+echo "Length of string $string is : $length"
+
---
+++ Adv/02_quotes.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+variable="Hello"
+
+echo "Double quotes: $variable"
+echo 'Single quotes: $variable'
+echo No quotes: $variable
+echo "Both Quotes: '$variable'"
